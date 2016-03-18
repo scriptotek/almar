87238eca47668057597c2e7377c2af4895531853
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-03-18 10:48:43 +0100

    Python 3.x-støtte

--- lokar.py
+++ lokar.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from __future__ import print_function
 import requests
 import xml.etree.ElementTree as ET
 
@@ -13,11 +14,11 @@ ns = {'e20' : 'http://explain.z3950.org/dtd/2.0/',
 indexes = root.findall('.//{http://explain.z3950.org/dtd/2.0/}index')
 len(indexes)
 
-print '%40s %s' % ('NAME', 'DESCRIPTION')
+print('%40s %s' % ('NAME', 'DESCRIPTION'))
 for index in indexes:
     title = index.find('e21:title' , ns).text
     name = index.find('.//e20:name' , ns).text
-    print ' %40s %s' % (name,title)
+    print(' %40s %s' % (name,title))
     
 searchUrl='https://sandbox02-eu.alma.exlibrisgroup.com/view/sru/47BIBSYS_UBO'
 mineparametre={
@@ -39,35 +40,35 @@ len(records)
 for n, record in enumerate(records):
     
     title = record.find('./datafield[@tag="245"]/subfield[@code="a"]').text.encode('utf-8')
-    print n, title
+    print(n, title)
     
     emner = record.findall('./datafield[@tag="650"]')
     for emne in emner:
             if emne.find('subfield[@code="2"]') is not None and emne.find('subfield[@code="2"]').text == 'noubomn':
-                print ' - ', emne.find('subfield[@code="a"]').text.encode('utf-8')
+                print(' - ', emne.find('subfield[@code="a"]').text.encode('utf-8'))
                 if emne.find('subfield[@code="a"]').text == u'Monstre':
-                    # print ' ---> Ja, monstre!'
+                    # print(' ---> Ja, monstre!')
                     emne.find('subfield[@code="a"]').text = u'Monsterbibliotekarer'
                 elif emne.find('subfield[@code="a"]').text == u'Mønstre':
-                    print ' --> Nei, mønstre!'
+                    print(' --> Nei, mønstre!')
                     
             elif emne.find('subfield[@code="2"]') is not None and emne.find('subfield[@code="2"]').text != 'noubomn':
-                print ' - ', emne.find('subfield[@code="a"]').text.encode('utf-8'), ' : ', emne.find('subfield[@code="2"]').text.encode('utf-8')
+                print(' - ', emne.find('subfield[@code="a"]').text.encode('utf-8'), ' : ', emne.find('subfield[@code="2"]').text.encode('utf-8'))
                 
             elif emne.find('subfield[@code="2"]') is None and record.findall('./datafield[@ind2="0"]'):
-                print ' - ', emne.find('subfield[@code="a"]').text, ' : LCSH '
+                print(' - ', emne.find('subfield[@code="a"]').text, ' : LCSH ')
                 
             elif emne.find('subfield[@code="2"]') is None and record.findall('./datafield[@ind2="2"]'):
-                print ' - ', emne.find('subfield[@code="a"]').text, ' : MeSH '
+                print(' - ', emne.find('subfield[@code="a"]').text, ' : MeSH ')
 # det vi mangler her er evt om det skal skrives noe for de andre verdiene i indikator 2, jeg klarte ikke hente ut verdien, 
 # bare sjekke om det var noe spesielt. Men dette er kanskje bare artigtesting på veien, evt også for å holde øye med om vi
 # klarer å la de andre emneordene være i fred...
                 
             elif emne.find('subfield[@code="2"]') is None:
-                print ' -  UKJENT : ', emne.find('subfield[@code="a"]').text
+                print(' -  UKJENT : ', emne.find('subfield[@code="a"]').text)
                 
     frie = record.findall('./datafield[@tag="653"]')
     for emne in frie:
-        print u' -  FRITT NØKKELORD : '.encode('utf-8'), emne.find('subfield[@code="a"]').text.encode('utf-8')
+        print(u' -  FRITT NØKKELORD : '.encode('utf-8'), emne.find('subfield[@code="a"]').text.encode('utf-8'))
         
-# print ET.tostring(record)
+# print(ET.tostring(record))
