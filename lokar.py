64109505dc74a58204af661200cf2cd9a741d11f
Heidi Konestabo <heidi.sjursen@gmail.com>
2016-05-03 10:17:39 +0200

    Vi har laget det sånn at vi kan få input fra brukeren

--- lokar.py
+++ lokar.py
@@ -1,33 +1,40 @@
 # coding=utf-8
 from __future__ import print_function
+from six.moves import input
 import requests
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET 
+
+gammelord = input('Det gamle emneordet: ')
+nyord = input('Det nye emneordet: ')
 
 ns = {'e20' : 'http://explain.z3950.org/dtd/2.0/',
      'e21' : 'http://explain.z3950.org/dtd/2.1/'}
     
-searchUrl='https://sandbox02-eu.alma.exlibrisgroup.com/view/sru/47BIBSYS_UBO'
+searchUrl='https://bibsys-k.alma.exlibrisgroup.com/view/sru/47BIBSYS_UBO'
 mineparametre={
     'version': '1.2',
             'operation': 'searchRetrieve',
-            'query': 'alma.subjects=Monstre',
-            'maximumRecords': '20',
+            'query': 'alma.subjects=' + gammelord,
+            'maximumRecords': '50',
     }
 
 response = requests.get(searchUrl, params=mineparametre)
 root = ET.fromstring(response.text.encode('utf-8'))
 records = root.findall('.//record')
 
+mms_ids = []
+print(len(records))
 for n, record in enumerate(records):    
-    title = record.find('./datafield[@tag="245"]/subfield[@code="a"]').text.encode('utf-8')
-    print(n, title)
+    mms_id = record.find('./controlfield[@tag="001"]').text
     
     emner = record.findall('./datafield[@tag="650"]')
     for emne in emner:
         if emne.find('subfield[@code="2"]') is not None and emne.find('subfield[@code="2"]').text == 'noubomn':
             print(' - ', emne.find('subfield[@code="a"]').text.encode('utf-8'))
-            if emne.find('subfield[@code="a"]').text == u'Monstre':
-                emne.find('subfield[@code="a"]').text = u'Monsterbibliotekarer'
-                    
+            if emne.find('subfield[@code="a"]').text == gammelord:
+                mms_ids.append(mms_id)
+                emne.find('subfield[@code="a"]').text = nyord
+
+print('Poster som vil bli endret: {:d}'.format(len(mms_ids)))                    
         
 # print(ET.tostring(record))
